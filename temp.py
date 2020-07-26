84fdfc06e92b7db25e6fce4e1b0537521c63f38c
yeaung <yeaung@DESKTOP-4MELPK3.ya>
2020-07-26 23:52:04 +0630

    fist commit

---
+++ temp.py
@@ -0,0 +1,55 @@
+import imageio
+import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+# import tensorflow.compat.v1 as tf
+# from nst_utils import load_vgg_model
+
+
+
+# image = imageio.imread('images/louvre_small.jpg')
+# print(image.shape)
+
+# mat = scipy.io.loadmat('pretrained-model/imagenet-vgg-verydeep-19.mat')
+# print(mat.keys())
+
+# model = load_vgg_model('pretrained-model/imagenet-vgg-verydeep-19.mat')
+# print(model['input'].assign())
+# dd = tf.Variable(np.array([[0,0]]),dtype='float32')
+# c1 = tf.constant(np.array([[1],[2]]))
+# d1 = tf.matmul(dd,c1)
+# print(dd)
+# dd.assign(np.array([[1,2]]))
+# print(dd)
+
+# mat = scipy.io.loadmat('image.mat')
+# plt.imshow(mat['image'][0,:,:,:])
+# plt.show()
+# i = mpimg.imread('images/content.jpeg')
+# plt.imshow(i)
+# plt.show
+
+# draw the figure so the animations will work
+fig = plt.gcf()
+fig.show()
+fig.canvas.draw()
+i = 0
+haha = [0]
+haha2 = [100]
+while True:
+    # compute something
+    haha.append(i)
+    haha2.append(100-i)
+    plt.subplot(1,2,1)
+    plt.plot(haha) # plot something
+
+    plt.subplot(1,2,2)
+    plt.plot(haha2)
+    
+    # update canvas immediately
+    #plt.xlim([0, 100])
+    plt.ylim([0, 100])
+    #plt.pause(0.01)  # I ain't needed!!!
+    fig.canvas.draw()
+    i+=1
